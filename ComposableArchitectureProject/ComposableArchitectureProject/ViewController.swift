066af7cc503feb06d03c391ce44f34617189fabb
Mark Jones <mjones@letsgetchecked.com>
2023-06-05 15:22:35 +0100

    Extract composable domain and reducer to Feature file

--- ComposableArchitectureProject/ComposableArchitectureProject/ViewController.swift
+++ ComposableArchitectureProject/ComposableArchitectureProject/ViewController.swift
@@ -8,30 +8,6 @@
 import UIKit
 import ComposableArchitecture
 
-// MARK: Feature - Defining the core domain of the App OR Feature
-// the domain and the reducer model our business logic for the App OR feature
-
-struct AppState {
-    
-}
-
-enum AppAction {
-    
-}
-
-struct AppEnvironment {  // dependencies
-    
-}
-
-// the appReducer is Generic over AppState, AppAction, AppEnvironment
-// reducer is responsible for all hte business logic of hte application OR feature
-let appReducer = AnyReducer<AppState, AppAction, AppEnvironment> { state, action, environment in
-    switch action {
-    default:
-        print("MJ Here")
-    }
-}
-
 // MARK: ViewController
 
 class ViewController: UIViewController {
@@ -51,5 +27,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .cyan
     }
+}
+
+extension ViewController {
+    
 }
 
