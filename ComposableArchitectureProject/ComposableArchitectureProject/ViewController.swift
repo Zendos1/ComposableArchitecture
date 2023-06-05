396f6aaa6f0c1b496ee79bf1b9910d57afae3e59
Mark Jones <mjones@letsgetchecked.com>
2023-06-05 14:51:37 +0100

    Update to latest composable library, add feature domain and reducer

--- ComposableArchitectureProject/ComposableArchitectureProject/ViewController.swift
+++ ComposableArchitectureProject/ComposableArchitectureProject/ViewController.swift
@@ -6,9 +6,47 @@
 //
 
 import UIKit
+import ComposableArchitecture
+
+// MARK: Feature - Defining the core domain of the App OR Feature
+// the domain and the reducer model our business logic for the App OR feature
+
+struct AppState {
+    
+}
+
+enum AppAction {
+    
+}
+
+struct AppEnvironment {  // dependencies
+    
+}
+
+// the appReducer is Generic over AppState, AppAction, AppEnvironment
+// reducer is responsible for all hte business logic of hte application OR feature
+let appReducer = AnyReducer<AppState, AppAction, AppEnvironment> { state, action, environment in
+    switch action {
+    default:
+        print("MJ Here")
+    }
+}
+
+// MARK: ViewController
 
 class ViewController: UIViewController {
-
+    // Store is generic over AppState & AppAction
+    var store: Store<AppState, AppAction>
+   
+    init(store: Store<AppState, AppAction>) {
+        self.store = store
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
